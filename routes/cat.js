d235f76812209280b8a1fb62805b4f16873ff9e1
chiete1362-blip <chieted1362@gmail.com>
2025-11-28 15:08:51 +0900

    add cat api

--- routes/cat.js
+++ routes/cat.js
@@ -3,12 +3,11 @@ var router = express.Router();
 const request = require('request');
 
 router.get('/', async(req, res) =>{
-	request('https://api.thecatapi.com/v1/images/search', function(error, response, body) {
-		if(!error&& response.statusCode== 200) {
-			const data= JSON.parse(body);
-			res.json(data);
-		}
-	});
+request('https://api.thecatapi.com/v1/images/search', function(error, response, body) {
+if(!error&& response.statusCode == 200) {
+const data = JSON.parse(body);
+res.json(data);
+}
+});
 })
-
-module.exports= router;
+module.exports = router;
